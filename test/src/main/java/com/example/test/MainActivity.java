9a966474f50a7df0ed2ab9105f4ea62182323f42
Ashok <ashokraj@appinessworld.com>
2019-09-18 12:23:03 +0530

    Initial commit

---
+++ test/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.test;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            int id = item.getItemId();
+            if (id == R.id.navigation_home) {
+                mTextMessage.setText(R.string.title_home);
+                return true;
+            } else if (id == R.id.navigation_dashboard) {
+                mTextMessage.setText(R.string.title_dashboard);
+                return true;
+            } else if (id == R.id.navigation_notifications) {
+                mTextMessage.setText(R.string.title_notifications);
+                return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
